87a53782bd6769bc0c012b785654b1c7974ce081
yoshwitha <yoshwithakaram@gmail.com>
2025-10-24 19:25:24 +0530

    Newcontract page is did

--- src/pages/Newcontract.jsx
+++ src/pages/Newcontract.jsx
@@ -16,16 +16,36 @@ function Newcontract() {
                     <div className="btn btn-light px-5">Air</div>
                 </div>
                 <div className="newcontract-top-right">
-                    Hi
+                    <h4>Inco Terms</h4>
+                    <div className='dropdown px-5'>
+                    <Form.Select aria-label="Default select example">
+                        <option>Select </option>
+                        <option value="1">FOB</option>
+                        <option value="2">CFR</option>
+                        <option value="3">CIF</option>
+                    </Form.Select>
+                    </div>
                 </div>
                 <div className="newcontract-middle-left">
-                    Hi
+                    <h4>Purchase Order</h4>
                 </div>
                 <div className="newcontract-middle-right">
-                    Hi
+                    <h4>Select Explorar</h4>
+                    <div className='dropdown px-5'>
+                    <Form.Select aria-label="Default select example">
+                        <option>Select </option>
+                        <option value="1">Exporter 2</option>
+                        <option value="2">Test User19</option>
+                        <option value="3">Verve Greens</option>
+                    </Form.Select>
+                    </div>
                 </div>
                 <div className="newcontract-bottom">
-                    Hi
+                    <div className='buttons'>
+                        <div className='btn btn-light px-5'>Cancel</div>
+                        <div className='btn btn-secondary px-5'>Save</div>
+                        <div className='btn btn-secondary px-5'>Save&Send</div>
+                    </div>
                 </div>
             </div>
         </div>
